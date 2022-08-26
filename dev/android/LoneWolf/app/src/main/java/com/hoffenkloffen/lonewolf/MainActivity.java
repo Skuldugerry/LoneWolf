05283c4b3a3086cd43b71d87801e2d9230e0ccb8
Hillary Chemiron <chemiron.hillary@student.moringaschool.com>
2022-08-26 13:54:28 +0300

    changes

--- dev/android/LoneWolf/app/src/main/java/com/hoffenkloffen/lonewolf/MainActivity.java
+++ dev/android/LoneWolf/app/src/main/java/com/hoffenkloffen/lonewolf/MainActivity.java
@@ -44,3 +44,19 @@ public class MainActivity extends RoboActivity {
         startActivity(intent);
     }
 }
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+    }
+}
